337ad0a8e65a898f177d97788f3d19ab8615f90f
Benjamin <bsinglet@vt.edu>
2020-03-13 00:16:48 -0400

    Modified this heavily to place all API calls in functions, and create a main function that interactively allows the user to toggle as many privileges as they want.

--- modify_token_privileges.py
+++ modify_token_privileges.py
@@ -1,10 +1,10 @@
 """
  Filename: modify_token_privileges.py
- Description: Given a window title, gets the process token and toggles
- SEDebugPrivilege.
+ Description: Given a window title, gets the process token and lets the user
+ toggle any privileges they choose.
 
- Modified from Brandon Dennis' "Hacking Windows API With Python" course on
- Udemy.
+ Heavily modified from Brandon Dennis' "Hacking Windows API With Python" course
+ on Udemy.
  Created by: Benjamin M. Singleton
  Created: 03-12-2020
 """
@@ -14,11 +14,6 @@ import ctypes
 
 # Import Python -> Windows Types from ctypes
 from ctypes.wintypes import DWORD
-
-# Grab a handle to kernel32.dll & USer32.dll & Advapi32.dll
-k_handle = ctypes.WinDLL("Kernel32.dll")
-u_handle = ctypes.WinDLL("User32.dll")
-a_handle = ctypes.WinDLL("Advapi32.dll")
 
 # Access Rights
 PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xFFF)
@@ -82,123 +77,181 @@ class TOKEN_PRIVILEGES(ctypes.Structure):
     ]
 
 
-# Grab The Windows Name from User32
-lpWindowName = ctypes.c_char_p(input("Enter Window Name To Hook Into: ").encode('utf-8'))
+def find_window_a(u_handle, k_handle):
+    # Grab The Windows Name from User32
+    lpWindowName = ctypes.c_char_p(input("Enter Window Name To Hook Into: ").encode('utf-8'))
 
-# Grab a Handle to the Process
-hWnd = u_handle.FindWindowA(None, lpWindowName)
+    # Grab a Handle to the Process
+    hWnd = u_handle.FindWindowA(None, lpWindowName)
 
-# Check to see if we have the Handle
-if hWnd == 0:
-    print("[ERROR] Could Not Grab Handle! Error Code: {0}".format(k_handle.GetLastError()))
-    exit(1)
-else:
-    print("[INFO] Grabbed Handle...")
+    # Check to see if we have the Handle
+    if hWnd == 0:
+        print("[ERROR] Could Not Grab Handle! Error Code: {0}".format(k_handle.GetLastError()))
+        exit(1)
+    else:
+        print("[INFO] Grabbed Handle...")
+    return hWnd
 
-# Get the PID of the process at the handle
-lpdwProcessId = ctypes.c_ulong()
 
-# We use byref to pass a pointer to the value as needed by the API Call
-response = u_handle.GetWindowThreadProcessId(hWnd, ctypes.byref(lpdwProcessId))
+def get_window_thread_process_id(hWnd, u_handle, k_handle):
+    # Get the PID of the process at the handle
+    lpdwProcessId = ctypes.c_ulong()
 
-# Check to see if the call Completed
-if response == 0:
-    print("[ERROR] Could Not Get PID from Handle! Error Code: {0}".format(k_handle.GetLastError()))
-else:
-    print("[INFO] Found PID...")
+    # We use byref to pass a pointer to the value as needed by the API Call
+    response = u_handle.GetWindowThreadProcessId(hWnd, ctypes.byref(lpdwProcessId))
 
-# Opening the Process by PID with Specific Access
-dwDesiredAccess = PROCESS_ALL_ACCESS
-bInheritHandle = False
-dwProcessId = lpdwProcessId
+    # Check to see if the call Completed
+    if response == 0:
+        print("[ERROR] Could Not Get PID from Handle! Error Code: {0}".format(k_handle.GetLastError()))
+    else:
+        print("[INFO] Found PID...")
+    return lpdwProcessId
 
-# Calling the Windows API Call to Open the Process
-hProcess = k_handle.OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId)
 
-# Check to see if we have a valid Handle to the process
-if hProcess <= 0:
-    print("[ERROR] Could Not Grab Privileged Handle! Error Code: {0}".format(k_handle.GetLastError()))
-else:
-    print("[INFO] Privileged Handle Opened...")
+def open_process(lpdwProcessId, k_handle):
+    # Opening the Process by PID with Specific Access
+    dwDesiredAccess = PROCESS_ALL_ACCESS
+    bInheritHandle = False
+    dwProcessId = lpdwProcessId
 
-# Open a Handle to the Process's Token Directly
-ProcessHandle = hProcess
-DesiredAccess = TOKEN_ALL_ACCESS
-TokenHandle = ctypes.c_void_p()
+    # Calling the Windows API Call to Open the Process
+    hProcess = k_handle.OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessId)
 
-# Issue the API Call
-response = k_handle.OpenProcessToken(ProcessHandle, DesiredAccess, ctypes.byref(TokenHandle))
+    # Check to see if we have a valid Handle to the process
+    if hProcess <= 0:
+        print("[ERROR] Could Not Grab Privileged Handle! Error Code: {0}".format(k_handle.GetLastError()))
+    else:
+        print("[INFO] Privileged Handle Opened...")
+    return hProcess
 
-# Handle an Error
-if response > 0:
-    print("[INFO] Handle to Process Token Created! Token: {0}".format(TokenHandle))
-else:
-    print("[ERROR] Could Not Grab Privileged Handle to Token! Error Code: {0}".format(k_handle.GetLastError()))
 
-# Check to see if we have SEDebugPrivilege
+def open_process_token(hProcess, k_handle):
+    # Open a Handle to the Process's Token Directly
+    ProcessHandle = hProcess
+    DesiredAccess = TOKEN_ALL_ACCESS
+    TokenHandle = ctypes.c_void_p()
+
+    # Issue the API Call
+    response = k_handle.OpenProcessToken(ProcessHandle, DesiredAccess, ctypes.byref(TokenHandle))
+
+    # Handle an Error
+    if response > 0:
+        print("[INFO] Handle to Process Token Created! Token: {0}".format(TokenHandle))
+    else:
+        print("[ERROR] Could Not Grab Privileged Handle to Token! Error Code: {0}".format(k_handle.GetLastError()))
+        exit(1)  # no point in going on
+    return TokenHandle
+
+# Check to see if we have the given privilege
 # First use the LookupPrivilegeValue API Call to get the LUID based on the String Privilege name
 
-# Setup a PRIVILEGE_SET for the PrivilegeCheck Call to be used later - We need the LUID to be used
-# We will reference it later as well
-requiredPrivileges = PRIVILEGE_SET()
-requiredPrivileges.PrivilegeCount = 1  # We are only looking at 1 Privilege at a time here
-requiredPrivileges.Privileges = LUID_AND_ATTRIBUTES()  # Setup a new LUID_AND_ATTRIBUTES
-requiredPrivileges.Privileges.Luid = LUID()  # Setup a New LUID inside of the LUID_AND_ATTRIBUTES structure
 
-# Params for Lookup API Call
-lpSystemName = None
-lpName = "SEDebugPrivilege"
+def lookup_privilege_value(privilege_name, a_handle, k_handle):
+    # Setup a PRIVILEGE_SET for the PrivilegeCheck Call to be used later - We need the LUID to be used
+    # We will reference it later as well
+    requiredPrivileges = PRIVILEGE_SET()
+    requiredPrivileges.PrivilegeCount = 1  # We are only looking at 1 Privilege at a time here
+    requiredPrivileges.Privileges = LUID_AND_ATTRIBUTES()  # Setup a new LUID_AND_ATTRIBUTES
+    requiredPrivileges.Privileges.Luid = LUID()  # Setup a New LUID inside of the LUID_AND_ATTRIBUTES structure
 
-# We now issue the Call to configure the LUID with the Systems Value of that Privilege
-response = a_handle.LookupPrivilegeValueW(lpSystemName, lpName, ctypes.byref(requiredPrivileges.Privileges.Luid))
+    # Params for Lookup API Call
+    lpSystemName = None
+    lpName = privilege_name
 
-# Handle an Error
-if response > 0:
-    print("[INFO] Lookup For SEDebugPrivilege Worked...")
-else:
-    print("[ERROR] Lookup for SEDebugPrivilege Failed! Error Code: {0}".format(k_handle.GetLastError()))
+    # We now issue the Call to configure the LUID with the Systems Value of that Privilege
+    response = a_handle.LookupPrivilegeValueW(lpSystemName, lpName, ctypes.byref(requiredPrivileges.Privileges.Luid))
 
-# Now that our LUID is setup and pointing to the correct Privilege we can check to see if its enabled
-pfResult = ctypes.c_long()
+    # Handle an Error
+    if response > 0:
+        print("[INFO] Privilege Lookup Worked...")
+    else:
+        print("[ERROR] Privilege Lookup Failed! Error Code: {0}".format(k_handle.GetLastError()))
+        raise Exception()
 
-response = a_handle.PrivilegeCheck(TokenHandle, ctypes.byref(requiredPrivileges), ctypes.byref(pfResult))
+    # if the privilege doesn't exist on the machine the call will succeed but the
+    # LUID will be all zeroes
+    if requiredPrivileges.Privileges.Luid.HighPart == 0 and requiredPrivileges.Privileges.Luid.LowPart == 0:
+        print('LookupPrivilegeValue() call succeeded, but privilege was not found.')
+        raise Exception()
 
-# Handle an Error
-if response > 0:
-    print("[INFO] PrivilegeCheck Worked...")
-else:
-    print("[ERROR] PrivilegeCheck Failed! Error Code: {0}".format(k_handle.GetLastError()))
+    return requiredPrivileges
 
-# We can check pfResult to see if our Privilege is enabled or not
-if pfResult:
-    print("[INFO] Privilege SEDebugPrivilege is Enabled...")
-    requiredPrivileges.Privileges.Attributes = SE_PRIVILEGE_DISABLED  # Disable if its currently Enabled
-else:
-    print("[INFO] Privilege SEDebugPrivilege is NOT Enabled...")
-    requiredPrivileges.Privileges.Attributes = SE_PRIVILEGE_ENABLED  # Enable if currently Disabled
 
-# We will not attempt to modify the selected Privilege in the Token
-DisableAllPrivileges = False
-NewState = TOKEN_PRIVILEGES()
-BufferLength = ctypes.sizeof(NewState)
-PreviousState = ctypes.c_void_p()
-ReturnLength = ctypes.c_void_p()
+def privilege_check(TokenHandle, requiredPrivileges, a_handle, k_handle):
+    # Now that our LUID is setup and pointing to the correct Privilege we can check to see if its enabled
+    pfResult = ctypes.c_long()
 
-# Configure Token Privileges
-NewState.PrivilegeCount = 1
-# we can reuse this LUID because it's the same privilege on the same machine
-NewState.Privileges = requiredPrivileges.Privileges
+    response = a_handle.PrivilegeCheck(TokenHandle, ctypes.byref(requiredPrivileges), ctypes.byref(pfResult))
 
-response = a_handle.AdjustTokenPrivileges(
-    TokenHandle,
-    DisableAllPrivileges,
-    ctypes.byref(NewState),
-    BufferLength,
-    ctypes.byref(PreviousState),
-    ctypes.byref(ReturnLength))
+    # Handle an Error
+    if response > 0:
+        print("[INFO] PrivilegeCheck Worked...")
+    else:
+        print("[ERROR] PrivilegeCheck Failed! Error Code: {0}".format(k_handle.GetLastError()))
 
-# Handle an Error
-if response > 0:
-    print("[INFO] AdjustTokenPrivileges Flipped Privilege...")
-else:
-    print("[ERROR] AdjustTokenPrivileges Failed! Error Code: {0}".format(k_handle.GetLastError()))
+    # We can check pfResult to see if our Privilege is enabled or not
+    if pfResult:
+        print("[INFO] Privilege is Enabled...")
+        requiredPrivileges.Privileges.Attributes = SE_PRIVILEGE_DISABLED  # Disable if its currently Enabled
+    else:
+        print("[INFO] Privilege is NOT Enabled...")
+        requiredPrivileges.Privileges.Attributes = SE_PRIVILEGE_ENABLED  # Enable if currently Disabled
+    return requiredPrivileges
+
+
+def adjust_token_privilege(requiredPrivileges, TokenHandle, a_handle, k_handle):
+    # We will now attempt to modify the selected Privilege in the Token
+    DisableAllPrivileges = False
+    NewState = TOKEN_PRIVILEGES()
+    BufferLength = ctypes.sizeof(NewState)
+    PreviousState = ctypes.c_void_p()
+    ReturnLength = ctypes.c_void_p()
+
+    # Configure Token Privileges
+    NewState.PrivilegeCount = 1
+    # we can reuse this LUID because it's the same privilege on the same machine
+    NewState.Privileges = requiredPrivileges.Privileges
+
+    response = a_handle.AdjustTokenPrivileges(
+        TokenHandle,
+        DisableAllPrivileges,
+        ctypes.byref(NewState),
+        BufferLength,
+        ctypes.byref(PreviousState),
+        ctypes.byref(ReturnLength))
+
+    # Handle an Error
+    if response > 0:
+        print("[INFO] AdjustTokenPrivileges Flipped Privilege...")
+    else:
+        print("[ERROR] AdjustTokenPrivileges Failed! Error Code: {0}".format(k_handle.GetLastError()))
+
+
+def toggle_privilege(privilege_name, TokenHandle, a_handle, k_handle):
+    requiredPrivileges = lookup_privilege_value(privilege_name, a_handle, k_handle)
+    requiredPrivileges = privilege_check(TokenHandle, requiredPrivileges, a_handle, k_handle)
+    adjust_token_privilege(requiredPrivileges, TokenHandle, a_handle, k_handle)
+
+
+def main():
+    # Grab a handle to kernel32.dll & USer32.dll & Advapi32.dll
+    k_handle = ctypes.WinDLL("Kernel32.dll")
+    u_handle = ctypes.WinDLL("User32.dll")
+    a_handle = ctypes.WinDLL("Advapi32.dll")
+    hWnd = find_window_a(u_handle, k_handle)
+    lpdwProcessId = get_window_thread_process_id(hWnd, u_handle, k_handle)
+    hProcess = open_process(lpdwProcessId, k_handle)
+    TokenHandle = open_process_token(hProcess, k_handle)
+
+    # let the user check as many privileges as possible
+    while True:
+        privilege_name = input('Check which privilege: ').strip()
+        try:
+            toggle_privilege(privilege_name, TokenHandle, a_handle, k_handle)
+        except:
+            print("Well, that didn't work.")
+        print()
+
+
+if __name__ == '__main__':
+    main()
